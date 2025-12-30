a3c956266b79e043594351979eec40c4b56c8655
Tanishq dhingra <dhingratanishq58@gmail.com>
2025-12-30 06:46:33 +0530

    Admin serices completed

---
+++ admin service/src/config/db.ts
@@ -0,0 +1,10 @@
+import dotenv from "dotenv";
+dotenv.config(); // MUST be at the top
+
+import { neon } from "@neondatabase/serverless";
+
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL is not defined");
+}
+
+export const sql = neon(process.env.DATABASE_URL);
